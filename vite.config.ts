2f05626a09bbba241ab5b6c9658fafdb29fe77a9
cristian2763 <124648244+cristian2763@users.noreply.github.com>
2025-12-09 00:37:55 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,11 @@ export default defineConfig(({ mode }) => {
         host: '0.0.0.0',
       },
       plugins: [react()],
+        export default defineConfig({
+  plugins: [react()],
+  base: './',   // <-- Agrega esta línea
+  // ... resto del código
+})
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
