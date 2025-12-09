3e0c5649699eb83a7106579b9a3b682cf0ae3911
cristian2763 <124648244+cristian2763@users.noreply.github.com>
2025-12-09 00:45:44 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,26 +3,27 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
-      },
-      plugins: [react()],
-        export default defineConfig({
-  plugins: [react()],
-  base: './',   // <-- Agrega esta línea
-  // ... resto del código
-})
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    // Esto es necesario para que funcione en GitHub Pages
+    base: './',
+    
+    plugins: [react()],
+    
+    server: {
+      port: 3000,
+      host: '0.0.0.0',
+    },
+    
+    define: {
+      'process.env.API_KEY': JSON.stringify(env.API_KEY),
+      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+    },
+    
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './'),
       }
-    };
+    }
+  };
 });
